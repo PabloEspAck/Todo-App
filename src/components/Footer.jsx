942a4d11cd83f20a11837a1f9ea86c8ee2df83c9
valen2996 <valentinabagnasco7@gmail.com>
2023-04-15 12:58:59 -0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Footer = () => {
+    return (
+      <footer>
+        <p>Full Stack Developer Bootcamp.
+        </p>
+        <p>
+          Coded by Pablo Espinosa Ackermann.
+        </p>
+      </footer>
+    );
+  };
+
+  
+  export default Footer;
